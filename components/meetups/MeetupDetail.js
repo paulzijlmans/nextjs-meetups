39f9f8d9cddefc5d42edc14efd8b4df189fa4b5a
Paul Zijlmans <paulzijlmans@hotmail.com>
2022-08-16 14:20:35 +0200

    Use Image component

--- components/meetups/MeetupDetail.js
+++ components/meetups/MeetupDetail.js
@@ -1,9 +1,17 @@
+import Image from 'next/image';
+
 import classes from './MeetupDetail.module.css';
 
 function MeetupDetail(props) {
   return (
     <section className={classes.detail}>
-      <img src={props.image} alt={props.title} />
+      <Image
+        src={props.image}
+        alt={props.title}
+        width={300}
+        height={150}
+        layout='responsive'
+      />
       <h1>{props.title}</h1>
       <address>{props.address}</address>
       <p>{props.description}</p>
--- components/meetups/MeetupItem.js
+++ components/meetups/MeetupItem.js
@@ -1,5 +1,8 @@
 import { useRouter } from 'next/router';
+import Image from 'next/image';
+
 import Card from '../ui/Card';
+
 import classes from './MeetupItem.module.css';
 
 function MeetupItem(props) {
@@ -13,7 +16,13 @@ function MeetupItem(props) {
     <li className={classes.item}>
       <Card>
         <div className={classes.image}>
-          <img src={props.image} alt={props.title} />
+          <Image
+            src={props.image}
+            alt={props.title}
+            width={300}
+            height={150}
+            layout='responsive'
+          />
         </div>
         <div className={classes.content}>
           <h3>{props.title}</h3>

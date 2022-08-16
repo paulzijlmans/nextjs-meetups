1e312ecc632ab76f83ddeac0d359ef63cfbe50f8
Paul Zijlmans <paulzijlmans@hotmail.com>
2022-08-16 11:52:15 +0200

    Meetup Detail

--- components/layout/MainNavigation.js
+++ components/layout/MainNavigation.js
@@ -3,7 +3,6 @@ import Link from 'next/link';
 import classes from './MainNavigation.module.css';
 
 function MainNavigation() {
-
   return (
     <header className={classes.header}>
       <div className={classes.logo}>React Meetups</div>

b546ae550d8bffe8d4ef91720c282f94feb1110d
Paul Zijlmans <paulzijlmans@hotmail.com>
2022-08-16 10:42:32 +0200

    HomePage

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,24 @@
+import MeetupList from '../components/meetups/MeetupList'
+
+const DUMMY_MEETUPS = [
+  {
+    id: 'm1',
+    title: 'A First Meetup',
+    image: 'https://upload.wikimedia.org/wikipedia/commons/d/d3/Stadtbild_M%C3%BCnchen.jpg',
+    address: 'Some address 5, 12345 Some City',
+    description: 'This is a first meetup',
+  },
+  {
+    id: 'm2',
+    title: 'A Second Meetup',
+    image: 'https://upload.wikimedia.org/wikipedia/commons/8/8b/Westfalenpark-100818-16757-Florian-Turm-cor.jpg',
+    address: 'Some address 10, 12345 Some City',
+    description: 'This is a second meetup',
+  },
+];
+
 function HomePage() {
-  return (
-    <div>HomePage</div>
-  )
+  return <MeetupList meetups={DUMMY_MEETUPS} />
 }
 
 export default HomePage
